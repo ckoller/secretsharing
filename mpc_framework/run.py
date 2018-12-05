d5d50009b39bf3cdb253b3049ed0212fdc07a60a
koller <christiankn@hotmail.dk>
2018-12-05 18:10:47 +0100

    dont run protocol triples if there is 0 mult gates

--- mpc_framework/run.py
+++ mpc_framework/run.py
@@ -102,7 +102,7 @@ class Server:
         print(config.player_count)
 
 if __name__ == '__main__':
-    setup = TestSetupLocalShell()
+    setup = Prod()
     setup.setup()
     print(config.players)
     print(config.all_players)
